9e34a8ebd15e464d4e3873989ac8c9b1be816156
Antony Roy <47647718+devotta@users.noreply.github.com>
2023-02-20 22:26:33 +0500

    Add files via upload

--- Antony_DatabaseORM_LAB4-DBMS/Question9-query.sql
+++ Antony_DatabaseORM_LAB4-DBMS/Question9-query.sql
@@ -1,4 +1,4 @@
---9)	Create a stored procedure to display supplier id, name, rating and Type_of_Service.
+-- 9)	Create a stored procedure to display supplier id, name, rating and Type_of_Service.
 -- For Type_of_Service, If rating =5, print “Excellent Service”,If rating >4 print “Good Service”,
 -- If rating >2 print “Average Service” else print “Poor Service”.
 
