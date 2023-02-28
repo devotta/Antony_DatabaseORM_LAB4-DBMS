8b9e7a6623110c6eb3e2c1c3855ab3d85b315464
Antony Roy <47647718+devotta@users.noreply.github.com>
2023-02-28 23:14:56 +0500

    Add files via upload

--- Antony_DatabaseORM_LAB4-DBMS/Question2-Insert-data.sql
+++ Antony_DatabaseORM_LAB4-DBMS/Question2-Insert-data.sql
@@ -6,7 +6,6 @@ INSERT INTO supplier(SUPP_NAME,SUPP_CITY,SUPP_PHONE) VALUES
 	("Appario Ltd.","Mumbai",'2589631470'),
 	("Knome products","Banglore",'9785462315'),
 	("Bansal Retails","Kochi",'8975463285'),
-	("Mittal Ltd.","Lucknow",'7898456532'),
 	("Mittal Ltd.","Lucknow",'7898456532');
 
 -- Insert data in customer table
@@ -42,25 +41,41 @@ INSERT INTO PRODUCT VALUES(12,"Train Your Brain","By Shireen Stephen",(SELECT CA
 -- Insert data in Supplier_pricing
 
 INSERT INTO supplier_pricing(PRO_ID,SUPP_ID,SUPP_PRICE)
-VALUES(3,5,30000),
-(1,2,1500),
- (3,5,30000),
- (5,1,3000),
- (2,3,2500),
- (4,1,1000),
- (12,2,780),
- (12,4,789),
- (3,1,31000),
- (1,5,1450),
- (4,2,999),
- (7,3,549),
- (7,4,529),
- (6,2,105),
- (6,1,99),
- (2,5,2999),
- (5,2,2999);
+VALUES((select pro_id from Product where Pro_name='ROG LAPTOP'),(select supp_id from Supplier where supp_name='Mittal Ltd.'),30000);
+INSERT INTO supplier_pricing(PRO_ID,SUPP_ID,SUPP_PRICE) Values
+((select pro_id from Product where Pro_name='GTA V'),(select supp_id from Supplier where supp_name='Appario Ltd.'),1500);
+INSERT INTO supplier_pricing(PRO_ID,SUPP_ID,SUPP_PRICE) Values
+ ((select pro_id from Product where Pro_name='ROG LAPTOP'),(select supp_id from Supplier where supp_name='Mittal Ltd.'),30000);
+INSERT INTO supplier_pricing(PRO_ID,SUPP_ID,SUPP_PRICE) Values
+ ((select pro_id from Product where Pro_name='HARRY POTTER'),(select supp_id from Supplier where supp_name='Rajesh Retails'),3000);
+ INSERT INTO supplier_pricing(PRO_ID,SUPP_ID,SUPP_PRICE) Values
+ ((select pro_id from Product where Pro_name='TSHIRT'),(select supp_id from Supplier where supp_name='Knome products'),2500);
+INSERT INTO supplier_pricing(PRO_ID,SUPP_ID,SUPP_PRICE) Values
+ ((select pro_id from Product where Pro_name='OATS'),(select supp_id from Supplier where supp_name='Rajesh Retails'),1000);
+INSERT INTO supplier_pricing(PRO_ID,SUPP_ID,SUPP_PRICE) Values
+ ((select pro_id from Product where Pro_name='Train Your Brain'),(select supp_id from Supplier where supp_name='Appario Ltd.'),780);
+ INSERT INTO supplier_pricing(PRO_ID,SUPP_ID,SUPP_PRICE) Values
+ ((select pro_id from Product where Pro_name='Train Your Brain'),(select supp_id from Supplier where supp_name='Bansal Retails'),789);
+ INSERT INTO supplier_pricing(PRO_ID,SUPP_ID,SUPP_PRICE) Values
+ ((select pro_id from Product where Pro_name='ROG LAPTOP'),(select supp_id from Supplier where supp_name='Rajesh Retails'),31000);
+INSERT INTO supplier_pricing(PRO_ID,SUPP_ID,SUPP_PRICE) Values
+ ((select pro_id from Product where Pro_name='GTA V'),(select supp_id from Supplier where supp_name='Mittal Ltd.'),1450);
+INSERT INTO supplier_pricing(PRO_ID,SUPP_ID,SUPP_PRICE) Values
+ ((select pro_id from Product where Pro_name='OATS'),(select supp_id from Supplier where supp_name='Appario Ltd.'),999);
+INSERT INTO supplier_pricing(PRO_ID,SUPP_ID,SUPP_PRICE) Values
+ ((select pro_id from Product where Pro_name='Boat Earphones'),(select supp_id from Supplier where supp_name='Knome products'),549);
+INSERT INTO supplier_pricing(PRO_ID,SUPP_ID,SUPP_PRICE) Values
+ ((select pro_id from Product where Pro_name='Boat Earphones'),(select supp_id from Supplier where supp_name='Bansal Retails'),529);
+INSERT INTO supplier_pricing(PRO_ID,SUPP_ID,SUPP_PRICE) Values
+ ((select pro_id from Product where Pro_name='MILK'),(select supp_id from Supplier where supp_name='Appario Ltd.'),105);
+INSERT INTO supplier_pricing(PRO_ID,SUPP_ID,SUPP_PRICE) Values
+ ((select pro_id from Product where Pro_name='MILK'),(select supp_id from Supplier where supp_name='Rajesh Retails'),99);
+INSERT INTO supplier_pricing(PRO_ID,SUPP_ID,SUPP_PRICE) Values
+ ((select pro_id from Product where Pro_name='TSHIRT'),(select supp_id from Supplier where supp_name='Mittal Ltd.'),2999);
+INSERT INTO supplier_pricing(PRO_ID,SUPP_ID,SUPP_PRICE) Values
+ ((select pro_id from Product where Pro_name='HARRY POTTER'),(select supp_id from Supplier where supp_name='Appario Ltd.'),2999);
 
- -
+ 
  -- Insert data in order table
 INSERT INTO `ORDER` VALUES (101,1500,"2021-10-06",2,1);
 INSERT INTO `ORDER` VALUES(102,1000,"2021-10-12",3,5);
@@ -70,17 +85,14 @@ INSERT INTO `ORDER` VALUES(105,3000,"2021-08-16",4,3);
 INSERT INTO `ORDER` VALUES(106,1450,"2021-08-18",1,9);
 INSERT INTO `ORDER` VALUES(107,789,"2021-09-01",3,7);
 INSERT INTO `ORDER` VALUES(108,780,"2021-09-07",5,6);
-INSERT INTO `ORDER` VALUES(109,3000,"2021-0-10",5,3);
--- Only 9 orders were able to insert data starting from order no: 101-109
-
--- The below data were not able to enter because of integrity constraints
-/*INSERT INTO `ORDER` VALUES(110,2500,"2021-09-10",2,4);
+INSERT INTO `ORDER` VALUES(109,3000,"2021-09-10",5,3);
+INSERT INTO `ORDER` VALUES(110,2500,"2021-09-10",2,4);
 INSERT INTO `ORDER` VALUES(111,1000,"2021-09-15",4,5);
 INSERT INTO `ORDER` VALUES(112,789,"2021-09-16",4,7);
 INSERT INTO `ORDER` VALUES(113,31000,"2021-09-16",1,8);
 INSERT INTO `ORDER` VALUES(114,1000,"2021-09-16",3,5);
 INSERT INTO `ORDER` VALUES(115,3000,"2021-09-16",5,3);
-INSERT INTO `ORDER` VALUES(116,99,"2021-09-17",2,14);*/
+INSERT INTO `ORDER` VALUES(116,99,"2021-09-17",2,14);
 
 
 
